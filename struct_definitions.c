64aa7f9049e3658422ee56856e6882de7863313f
Clara Oberle <clara.oberle01@estudiant.upf.edu>
2024-05-02 18:36:12 +0200

    started the structures

---
+++ struct_definitions.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define N 20 // max number of letters in the name
+
+typedef struct
+
+
+typedef struct{
+    char Name[N];
+    int life_points, attack_points, defense_points;
+    //skills
+}Character;
