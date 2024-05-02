f0d3cbb506a9a3d1aff49e25dd7834a8ce607a4e
Clara Oberle <clara.oberle01@estudiant.upf.edu>
2024-05-02 18:46:17 +0200

    char and skills struct

--- struct_definitions.c
+++ struct_definitions.c
@@ -3,12 +3,28 @@
 #include <string.h>
 
 #define N 20 // max number of letters in the name
-
-typedef struct
-
+#define M 200 //max number of letters for description
 
 typedef struct{
-    char Name[N];
+    char name[N];
+    char description[M];
+    int type; //if type = 0 --> temporarie modifier, else (type = 1) --> direct attack
+    int duration_turn; //how many turns the skill lasts
+    int modifier[3]; //modifier[0] = life_points, modifier[1] = attack_points, modifier[2] = defense_points 
+}Skill;
+/*
+--> Just to remember how it will have to be accessed printed later on
+Assign values to the name and description members
+    strcpy(mySkill.name, "Vine Bind");
+    strcpy(mySkill.description, "Traps the player with twisting vines, making them slower and easier to hit for one turn.");
+
+Printing the name and description
+    printf("Skill Name: %s\n", mySkill.name);
+    printf("Description: %s\n", mySkill.description);
+*/
+
+typedef struct{
+    char name[N];
     int life_points, attack_points, defense_points;
-    //skills
+    Skill skills[4]; //array of skills (of type Skill, the structure)
 }Character;
