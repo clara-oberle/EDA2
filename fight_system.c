58d52a7a969e646bce2dee003e8cdc67aebc01d7
Clara Oberle <clara.oberle01@estudiant.upf.edu>
2024-05-12 13:21:08 +0200

    queue in progress

--- fight_system.c
+++ fight_system.c
@@ -1,10 +1,55 @@
 #include "fight_system.h"
+#include "stdbool.h"
+#include <stdlib.h>
+#include <time.h>
+
+//function to get 0 or 1 --> If 0; char turn, if 1; enemy turn
+int getRandomZeroOrOne() {
+    return rand() % 2;
+}
+
+void init_queue(Queue *queue) {
+    queue->first = NULL;
+    queue->last = NULL;
+    queue->size = 0;
+}
+bool is_empty(Queue *queue){
+    return (queue->size == 0);
+}
 
 Queue *create_queue(Character *player, Enemy *enemy){
     // Create the queue to decide the turns
     // A generator should choose a fighter N times with 50-50 odds, enqueuing the turn generated at each draw
-    for(int i=0; i<MAX_TURNS; i++){
+    //allocate space for queue
+    Queue* queue = (Queue*)malloc(sizeof(Queue));
+    // Initialize the queue
+    init_queue(queue);
 
+    for(int i=0; i<MAX_TURNS; i++){
+        srand(time(NULL)); // Initialize random number generator with current time
+        int randomNumber = getRandomZeroOrOne(); // Generate either 0 or 1
+
+        //if random number is 0 --> char turn
+        if(randomNumber == 0){
+            if(queue->size == 0){
+                //queue is empty, we configure the queue first 
+                strcpy(queue->first->name, player->name);
+                queue->first->type = 0;
+                queue->first->next = NULL;
+
+                //we also configure the queue last, which is the head
+                strcpy(queue->last->name, player->name);
+                queue->last->type = 0;
+                queue->last->next = NULL;
+            }
+            else{
+                //queue is not empty
+
+            }
+        }
+        else{
+            //random numebr = 1 --> enemy turn
+        }
     }
 }
 
--- fight_system.h
+++ fight_system.h
@@ -1,21 +1,21 @@
 #ifndef FIGHT_SYSTEM_H
 #define FIGHT_SYSTEM_H
 
-#define MAX_TURNS 20
+#define MAX_TURNS 20 
 #include "struct_definitions.h"
 
 // Queue to decide turns:
-typedef struct{
-    Fighter *first;
-    Fighter *last;
-    int size;
-}Queue;
-
 typedef struct{
     char name[N];
     int type; // Character -> 0, Enemy -> 1;
     Fighter *next;
 }Fighter;
+
+typedef struct{
+    Fighter *first; //head
+    Fighter *last; //tail
+    int size; //size of the queue
+}Queue;
 
 // Stack to keep track of player's moves:
 typedef struct{
