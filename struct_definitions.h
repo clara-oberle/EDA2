cc80b45dd1a03114f7cad2a6e1546ec92da0b52e
zainkq <167971787+zainkq@users.noreply.github.com>
2024-05-19 10:52:51 +0200

    Update struct_definitions.h

--- struct_definitions.h
+++ struct_definitions.h
@@ -55,19 +55,5 @@ typedef struct{
     int num_options;
 } Decision;
 
-typedef struct GraphNode {
-    Scenario *scenario;
-    struct GraphNode *next;
-} GraphNode;
-
-typedef struct {
-    int num_scenarios;
-    GraphNode **adj_list;
-} Graph;
-
-Graph* create_graph(int num_scenarios);
-void add_edge(Graph *graph, int src, int dest);
-void print_graph(Graph *graph);
-
 
 #endif
